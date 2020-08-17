2577ef1a5b5c35905873be4a9a44b60099027a87
Kyle Puno <kylepuno@Kyles-MacBook-Pro.local>
2020-08-17 13:00:23 -0400

    added loader

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Gallery } from './Gallery'
 export { default as SearchBar } from './SearchBar'
+export { default as Loader } from './Loader'
--- src/container/MovieDetails.js
+++ src/container/MovieDetails.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react'
 import { searchMovieDetails } from '../helpers/api'
+import { Loader } from '../components'
 
 class MovieDetails extends Component {
   constructor() {
     super()
     this.state = {
       movie: {},
+      loading: true,
     }
   }
 
   componentDidMount() {
     const { match } = this.props
     searchMovieDetails(match.params.movieID)
-      .then(data => this.setState({ movie: data }))
+      .then(data => this.setState({ movie: data, loading: false }))
   }
 
   render() {
     const { original_title, tagline, overview, poster_path } = this.state.movie
+    const { loading } = this.state
+
     return (
-      <div className="poster">
-        <div className="description">
-          <h1>{original_title}</h1>
-          <h2>{tagline}</h2>
-          <p>{overview}</p>
-        </div>
-        <div className="image">
-          <img src={`http://image.tmdb.org/t/p/w500/${poster_path}`} alt={`Movie poster for ${original_title}`}/>
-        </div>
-      </div>
+      <>
+        {
+          loading ? <Loader /> :
+          <div className="poster">
+            <div className="description">
+              <h1>{original_title}</h1>
+              <h2>{tagline}</h2>
+              <p>{overview}</p>
+            </div>
+            <div className="image">
+              <img src={`http://image.tmdb.org/t/p/w500/${poster_path}`} alt={`Movie poster for ${original_title}`}/>
+            </div>
+          </div>
+        }
+      </>
     )
   }
 }
